2d14db1a8fbd25d861537dd4a817df1f65d73c24
Igor Lima <62576492+iguulima@users.noreply.github.com>
2024-08-28 23:18:06 -0300

    Tipos de dados

---
+++ TiposDados/TiposDados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Curso {
+    class Program {
+        static void Main(string[] args) {
+
+            bool completo = false; // Dados Boleanos
+            char genero = 'F'; // Dados com uma letra só, tem que estar entre aspas simples
+            byte n1 = 126; //Numeros que vao de 0 a 255
+            int n2 = 1000; // Numeros inteiros de ate 32bits
+            int n3 = 2147483647; 
+            long n4 = 2147483648L; // numero inteiro maior que int, de 64bits
+            float n5 = 4.5f; //decimal de ate 32 bits
+            double n6 = 4.5; // decimal de 64 bits
+            String nome = "Maria Green"; // texto em aspas duplas
+            Object obj1 = "Alex Brown"; // object é um tipo generico, pode receber qualquer tipo de variavei
+            Object obj2 = 4.5f;
+
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            
+
+
+        }
+    }
+}
--- TiposDados/TiposDados/obj/Release/net8.0/TiposDados.AssemblyInfo.cs
+++ TiposDados/TiposDados/obj/Release/net8.0/TiposDados.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TiposDados")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9398a9dff6ad6c468b70ac0a39e763f8492cad")]
-[assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
+[assembly: System.Reflection.AssemblyProductAttribute("TiposDados")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TiposDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
